ac2baa8e765c309d99643f563c84470711692f00
Mike Pirog <mike@kalamuna.com>
2015-07-15 00:29:42 -0400

    #257: Refactor to support decentralized usage

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 var path = require('path');
 var fs = require('fs');
 var _ = require('lodash');
-var taskOpts = require('./tasks.js');
+var taskOpts = require('./lib/tasks.js');
 
 // "Constants"
 var PLUGIN_NAME = 'kalabox-plugin-drush';
@@ -18,77 +18,9 @@ module.exports = function(kbox) {
 
   kbox.whenApp(function(app) {
 
-    // Helpers
-    /**
-     * Gets plugin conf from the appconfig or from CLI arg
-     **/
-    var getOpts = function(options) {
-      // Grab our options from config
-      var defaults = app.config.pluginConf[PLUGIN_NAME];
-      // Override any config coming in on the CLI
-      _.each(Object.keys(defaults), function(key) {
-        if (_.has(options, key) && options[key]) {
-          defaults[key] = options[key];
-        }
-      });
-      return defaults;
-    };
-
-    /**
-     * Runs a git command on the app data container
-     **/
-    var runDrushCMD = function(cmd, opts, done) {
-
-      // Run the drush command in the correct directory in the container if the
-      // user is somewhere inside the code directory on the host side.
-      // @todo: consider if this is better in the actual engine.run command
-      // vs here.
-
-      // Get current working directory.
-      var cwd = process.cwd();
-
-      // Get code root.
-      var codeRoot = app.config.codeRoot;
-
-      // Get the branch of current working directory.
-      // Run the drush command in the correct directory in the container if the
-      // user is somewhere inside the code directory on the host side.
-      // @todo: consider if this is better in the actual engine.run command
-      // vs here.
-      var workingDirExtra = '';
-      if (_.startsWith(cwd, codeRoot)) {
-        workingDirExtra = cwd.replace(codeRoot, '');
-      }
-      var codeDir = globalConfig.codeDir;
-      var workingDir = '/' + codeDir + workingDirExtra;
-
-      // Get drush version.
-      var drushVersion = (opts['drush-version'] === 'backdrush') ?
-        'backdrush' :
-        'drush' + opts['drush-version'];
-
-      // Image name.
-      var image = 'drush';
-
-      // Build create options.
-      var createOpts = kbox.util.docker.CreateOpts()
-        .workingDir(workingDir)
-        .env('DRUSH_VERSION', drushVersion)
-        .volumeFrom(app.dataContainerName)
-        .json();
-
-      // Build start options.
-      var startOpts = kbox.util.docker.StartOpts()
-        .bind(app.config.homeBind, '/ssh')
-        .bind(app.rootBind, '/src')
-        .json();
-
-      // Perform a container run.
-      return engine.run(image, cmd, createOpts, startOpts)
-      // Return.
-      .nodeify(done);
-
-    };
+    // Grab the clients
+    var Drush = require('./lib/drush.js');
+    var drush = new Drush(kbox, app);
 
     // Events
     // Install the drush container for our things
@@ -151,10 +83,10 @@ module.exports = function(kbox) {
       task.kind = 'delegate';
       task.options.push(taskOpts.drushVersion);
       task.func = function(done) {
-        var opts = getOpts(this.options);
+        var opts = drush.getOpts(this.options);
         var cmd = this.payload;
         cmd.unshift('@dev');
-        runDrushCMD(cmd, opts, done);
+        drush.cmd(cmd, opts, done);
       };
     });
 
